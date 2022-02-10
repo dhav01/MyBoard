a5405e3fda0fc26f11c4aa1c7e37a11a53e0803b
dhaval <dhagrawal0@gmail.com>
2022-02-10 12:51:38 +0530

    added socket.io connection

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 let express = require('express')
 let app = express()
 let httpServer = require('http').createServer(app)
-
 let io = require('socket.io')(httpServer)
+
+let connections = []
+
+io.on('connect', (socket) => {
+  connections.push(socket)
+  console.log(`${socket.id} has been connected`)
+
+  socket.on('disconnect', (reason) => {
+    console.log('realtime collaboration shut down...')
+    connections = connections.filter((cUser) => cUser.id !== socket.id)
+  })
+})
 
 app.use(express.static('public'))
 
--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
 let canvas = document.getElementById('canvas')
 let context = canvas.getContext('2d')
 
+var io = io.connect('http://localhost:3000/')
+
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
@@ -9,6 +11,7 @@ let y
 let mouseDown = false
 
 window.onmousedown = (e) => {
+  context.moveTo(x, y)
   mouseDown = true
 }
 
